3e2138c84ad48a419dcf44b24c4d33151337ca9e
Jon Whitman <jtw3909@gmail.com>
2023-01-04 01:38:38 -0800

    happy new year charts

--- record_catalog/client/src/components/ChartTwo.js
+++ record_catalog/client/src/components/ChartTwo.js
@@ -1,12 +1,12 @@
 import React, { useMemo, useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom'
-import { BarChart, Bar, CartesianGrid, XAxis, YAxis } from 'recharts';
+import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip, CustomTooltip, Legend } from 'recharts';
 const ChartTwo = () => {
     const {releaseYear} = useParams()
     const [releaseYearList, setReleaseYearList ] = useState([])
     const [list, setList] = useState([])
-    const [countList, setCountList] = useState([])
+    const [amountList, setAmountList] = useState([])
     useEffect(() =>{
         // get all records
         axios.get('http://localhost:8000/api/allRecords')
@@ -29,16 +29,16 @@ const ChartTwo = () => {
                 // if yes then increase the occurence by 1
                 arr2.forEach((k)=>{
                     if(k[key] === x[key]){
-                        k["occurrence"]++
+                        k["amount"]++
                     }
                 })
             }else{
                 // if not then create a new object, initialize
                 // it with the present iteration key's value and
-                // set the occurrence to 1
+                // set the amount to 1
                 let a = {}
                 a[key] = x[key]
-                a["occurrence"]= 1
+                a["amount"]= 1
                 arr2.push(a)
                 arr2.sort((a,b) => a.releaseYear - b.releaseYear)
             }
@@ -48,16 +48,28 @@ const ChartTwo = () => {
     let arr = list
     let key = "releaseYear"
     console.log(findOcc(arr, key))
+    const CustomTooltip = ({ active, payload, label }) => {
+        if (active && payload && payload.length) {
+        return (
+            <div className="bg-dark mx-auto" style={{padding: "5px"}}>
+                { payload[0].value > 1 ?  
+            <p className="label">{`${label} : ${payload[0].value}  records`}</p>
+        : <p className="label">{`${label} : ${payload[0].value}  record`}</p>
+        }
+            </div>
+        );
+        }}
         return (
             <div style={{display: "flex", justifyContent: "center", marginRight: "70px", marginTop: "50px"}}>
                 <BarChart width={1600} height={600} data={findOcc(arr, key)}>
-                    <Bar dataKey="occurrence" fill="cyan" />
                     <CartesianGrid stroke="#ccc" />
                     <XAxis dataKey="releaseYear" />
-                    <YAxis dataKey="occurrence" />
+                    <YAxis dataKey="amount" />
+                    <Tooltip content={<CustomTooltip />}/>
+                    <Legend  />
+                    <Bar dataKey="amount" fill="royalBlue" barSize={15}/>
                 </BarChart>
             </div>
         )
-    }
-
+}
 export default ChartTwo;
